5a846c7309fe343bdfe2af3c47a844567a9cd249
jiwon_lala <s26788761@naver.com>
2021-08-21 00:36:28 +0900

    #5 git add 기능 구현

--- Committer.js
+++ Committer.js
@@ -1,7 +1,9 @@
-const { execSync } = require("child_process");
+const { exec, execSync } = require("child_process");
 
 class Committer {
   static commit(file) {
+    //0. check cur dir is git directory
+
     //1. check file is valid_
     //first) file is located at this dir
     //second) check file's located dir also
@@ -16,6 +18,37 @@ class Committer {
 
     const getGitCommitCmdStr = `git commit -m "${getCommitMessage(file)}"`;
 
+    // const addStdout = exec(
+    //   `${getGitAddCmdStr(file)}`,
+    //   (error, stdout, stderr) => {
+    //     console.error(error);
+    //     console.error(stdout);
+    //     console.error(stderr);
+    //   }
+    // );
+
+    // try {
+    //   const addStdout = execSync(`${getGitAddCmdStr(file)}`);
+    //   console.log(addStdout.toString());
+    // } catch (e) {
+    //   console.log(e.status);
+    //   console.log(e.message);
+    //   console.log(e.stderr);
+    //   console.log(e.stdout);
+    // }
+
+    const addStdout = exec(
+      `${getGitAddCmdStr(file)}`,
+      (error, stdout, stderr) => {
+        console.error(error); //null
+        console.error(stdout); //""
+        console.error(stderr); //""
+      }
+    );
+    addStdout.on("exit", (code) => console.log(code)); //if success code value is 0
+
     //4. how to know git commit is succes?
   }
 }
+
+module.exports = Committer;
