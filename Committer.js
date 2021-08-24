f0f29ce1cabe880ba614782c7a1d384d1331192a
jiwon_lala <s26788761@naver.com>
2021-08-24 23:54:01 +0900

    #7 Fix console.log part to check for fail case

--- Committer.js
+++ Committer.js
@@ -14,19 +14,31 @@ class Committer {
     const addStdout = exec(
       `${getGitAddCmdStr(file)}`,
       (error, stdout, stderr) => {
-        console.error(error);
-        console.error(stdout);
-        console.error(stderr);
+        // console.error(error);  //this value is 'null'
+        // console.error(stdout); //this value is ''
+        // console.error(stderr); //this value is ''
       }
     );
-    addStdout.on("exit", (code) => console.log(code)); //if success code value is 0
+    addStdout.on("exit", (code) =>
+      code === 0
+        ? console.log(`'git add' command is successed. with exit code:${code}`) //if success code value is 0
+        : console.error(`'git add' command is failed. with exit code:${code}`)
+    );
 
     //3. git commit -m
     const getCommitMessage = (file) => `Add ${file}`;
     const getGitCommitCmdStr = `git commit -m "${getCommitMessage(file)}"`;
 
     const commitStdout = exec(getGitCommitCmdStr);
-    commitStdout.on("exit", (code) => console.log(code)); //if success code value is 0
+    commitStdout.on("exit", (code) =>
+      code === 0
+        ? console.log(
+            `'git commit' command is successed. with exit code:${code}`
+          ) //if success code value is 0
+        : console.error(
+            `'git commit' command is failed. with exit code:${code}`
+          )
+    );
 
     //4. how to know git commit is succes?
   }
